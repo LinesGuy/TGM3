c90ee91354a4511e5e5a118dc18d1753e47ba59d
linus <gudgeon.christopher@gmail.com>
2021-11-23 17:08:07 +0000

    lamkas a cute

--- TGM3/Art.cs
+++ TGM3/Art.cs
@@ -6,9 +6,25 @@ using System.Text;
 
 namespace TGM3 {
     static class Art {
-        public static Texture2D blockw;
+        public static Texture2D block;
+        public static Texture2D blockI;
+        public static Texture2D blockJ;
+        public static Texture2D blockL;
+        public static Texture2D blockO;
+        public static Texture2D blockS;
+        public static Texture2D blockT;
+        public static Texture2D blockZ;
+        public static Texture2D Grid;
         public static void Load(ContentManager content) {
-            blockw = content.Load<Texture2D>("blockw");
+            block = content.Load<Texture2D>("B  lock");
+            blockI = content.Load<Texture2D>("BlockI");
+            blockJ = content.Load<Texture2D>("BlockJ");
+            blockL = content.Load<Texture2D>("BlockL");
+            blockO = content.Load<Texture2D>("BlockO");
+            blockS = content.Load<Texture2D>("BlockS");
+            blockT = content.Load<Texture2D>("BlockT");
+            blockZ = content.Load<Texture2D>("BlockZ");
+            Grid = content.Load<Texture2D>("Grid");
         }
     }
 }
