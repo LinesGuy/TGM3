e95b2b595c2f29971f838719cc2a625ed2f41845
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-11-23 12:14:57 +0000

    DAS+ARR

--- TGM3/Playfield.cs
+++ TGM3/Playfield.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Diagnostics;
 
 namespace TGM3 {
     public static class Playfield {
@@ -14,6 +15,16 @@ namespace TGM3 {
         public static int Gravity = 1024;
         public static int PieceType;
         public static int PieceRotation = 0;
+        public static int CurrentDirection;
+        public static int ArrFrames;
+        public static int CurrentArrFrames;
+        public static int AreFrames;
+        public static int FramesUntilSpawn;
+        public static int LineAreFrames;
+        public static int DasFrames;
+        public static int CurrentDas;
+        public static int LockFrames;
+        public static int LineClearFrames;
         private static Random rand = new Random();
         public static int ClampRotation(int deltaRot) {
             if (PieceRotation + deltaRot >= 4)
@@ -25,6 +36,15 @@ namespace TGM3 {
         }
         public static void Initialize() {
             Grid = new int[(int)Size.Y, (int)Size.X];
+            CurrentDirection = 0;
+            ArrFrames = 1;
+            CurrentArrFrames = 0;
+            AreFrames = 4;
+            LineAreFrames = 4;
+            DasFrames = 6;
+            CurrentDas = 0;
+            LockFrames = 15;
+            LineClearFrames = 6;
             NewPiece();
         }
         public static bool CanMove(int deltaX, int deltaY, int deltaRot) {
@@ -168,12 +188,16 @@ namespace TGM3 {
             NewPiece(); // temp? idk xd
         }
         public static void Update() {
+            
+            #region Rotation input
             // CCW rotation
             if (Input.WasKeyJustDown(Keys.X))
                 TryKickMove(0, 0, 1);
             // CW rotation
             if (Input.WasKeyJustDown(Keys.Z))
                 TryKickMove(0, 0, -1);
+            #endregion
+            #region Soft/Hard drop
             // Soft drop
             if (Input.keyboard.IsKeyDown(Keys.Down))
                 Buildup += 65536; // 1G
@@ -183,15 +207,46 @@ namespace TGM3 {
                     continue;
                 LockPiece();
             }
-
-            // Left
-            if (Input.WasKeyJustDown(Keys.Left))
+            #endregion
+            #region Movement
+            // Basic left
+            if (Input.WasKeyJustDown(Keys.Left)) {
+                CurrentDirection = -1;
                 if (CanMove(-1, 0, 0))
                     PieceX--;
-            // Right
-            if (Input.WasKeyJustDown(Keys.Right))
+            }
+
+            // Basic right
+            if (Input.WasKeyJustDown(Keys.Right)) {
+                CurrentDirection = 1;
                 if (CanMove(1, 0, 0))
                     PieceX++;
+            }
+            // Release -> reset DAS
+            if ((Input.WasKeyJustUp(Keys.Left) && CurrentDirection == -1) || (Input.WasKeyJustUp(Keys.Right) && CurrentDirection == 1)) {
+                CurrentDirection = 0;
+                CurrentDas = 0;
+            }
+            if (CurrentDirection != 0) {
+                if (CurrentDas >= DasFrames) {
+                    if (ArrFrames == 0) {
+                        while (CanMove(CurrentDirection, 0, 0))
+                            PieceX += CurrentDirection;
+                    } else {
+                        CurrentArrFrames++;
+                        if (CurrentArrFrames >= ArrFrames) {
+                            TryMove(CurrentDirection, 0, 0);
+                            CurrentArrFrames = 0;
+                        }
+                    }
+                }
+                CurrentDas++;
+            }
+
+            
+            
+            #endregion
+            #region Gravity
             // Gravity
             Buildup += Gravity;
             while (Buildup >= 65536) {
@@ -199,6 +254,7 @@ namespace TGM3 {
                 if (CanMove(0, 1, 0))
                     PieceY++;
             }
+            #endregion
         }
         public static void Draw(SpriteBatch spriteBatch) {
             //for (int y = (int)Size.Y - visibleRows; y < Size.Y; y++) {
