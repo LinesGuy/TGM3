03babc09281d5f65758a0be48e297984373c2927
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-11-23 23:08:22 +0000

    Reupload lmao

---
+++ Src/GameRoot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TGM3 {
+    public class GameRoot : Game {
+        public static readonly Vector2 ScreenSize = new Vector2(1366, 768);
+        private readonly GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        public GameRoot() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+        protected override void Initialize() {
+            graphics.PreferredBackBufferWidth = (int)ScreenSize.X;
+            graphics.PreferredBackBufferHeight = (int)ScreenSize.Y;
+            graphics.ApplyChanges();
+            Tetris.Initialize();
+            base.Initialize();
+        }
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Art.Load(Content);
+        }
+        protected override void Update(GameTime gameTime) {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+            Input.Update();
+            Tetris.Update();
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            Tetris.Draw(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Src/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGM3 {
+    public static class Utils {
+        public static int ClampRotation(int currentPieceRotation, int deltaRotation) {
+            if (currentPieceRotation + deltaRotation >= 4)
+                return deltaRotation - 4;
+            else if (currentPieceRotation + deltaRotation < 0)
+                return deltaRotation + 4;
+            else
+                return deltaRotation;
+        }
+    }
+}
