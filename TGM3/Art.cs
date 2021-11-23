56ce97baec8ff657dfb9545a08a0bdf44983841c
linus <gudgeon.christopher@gmail.com>
2021-11-23 17:25:52 +0000

    Add proper textures

--- TGM3/Art.cs
+++ TGM3/Art.cs
@@ -6,24 +6,24 @@ using System.Text;
 
 namespace TGM3 {
     static class Art {
-        public static Texture2D block;
-        public static Texture2D blockI;
-        public static Texture2D blockJ;
-        public static Texture2D blockL;
-        public static Texture2D blockO;
-        public static Texture2D blockS;
-        public static Texture2D blockT;
-        public static Texture2D blockZ;
+        public static Texture2D Block;
+        public static Texture2D BlockI;
+        public static Texture2D BlockJ;
+        public static Texture2D BlockL;
+        public static Texture2D BlockO;
+        public static Texture2D BlockS;
+        public static Texture2D BlockT;
+        public static Texture2D BlockZ;
         public static Texture2D Grid;
         public static void Load(ContentManager content) {
-            block = content.Load<Texture2D>("Block");
-            blockI = content.Load<Texture2D>("BlockI");
-            blockJ = content.Load<Texture2D>("BlockJ");
-            blockL = content.Load<Texture2D>("BlockL");
-            blockO = content.Load<Texture2D>("BlockO");
-            blockS = content.Load<Texture2D>("BlockS");
-            blockT = content.Load<Texture2D>("BlockT");
-            blockZ = content.Load<Texture2D>("BlockZ");
+            Block = content.Load<Texture2D>("Block");
+            BlockI = content.Load<Texture2D>("BlockI");
+            BlockJ = content.Load<Texture2D>("BlockJ");
+            BlockL = content.Load<Texture2D>("BlockL");
+            BlockO = content.Load<Texture2D>("BlockO");
+            BlockS = content.Load<Texture2D>("BlockS");
+            BlockT = content.Load<Texture2D>("BlockT");
+            BlockZ = content.Load<Texture2D>("BlockZ");
             Grid = content.Load<Texture2D>("Grid");
         }
     }
